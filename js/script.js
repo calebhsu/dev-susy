3caf557c6f3ffbf744dadb047552d7858fd6dd1d
Caleb Hsu <chialo.hsu@colorado.edu>
2016-05-01 22:12:32 -0600

    Added more flowers woot

--- js/script.js
+++ js/script.js
@@ -21,6 +21,46 @@ $(document).ready(function(){
     $(".fl5").slideToggle("slow");
   });
 
+  $("#toggle6").click(function(){
+    $(".fl6").slideToggle("slow");
+  });
+
+  $("#toggle7").click(function(){
+    $(".fl7").slideToggle("slow");
+  });
+
+  $("#toggle8").click(function(){
+    $(".fl8").slideToggle("slow");
+  });
+
+  $("#toggle9").click(function(){
+    $(".fl9").slideToggle("slow");
+  });
+
+  $("#toggle10").click(function(){
+    $(".fl10").slideToggle("slow");
+  });
+
+  $("#toggle11").click(function(){
+    $(".fl11").slideToggle("slow");
+  });
+
+  $("#toggle12").click(function(){
+    $(".fl12").slideToggle("slow");
+  });
+
+  $("#toggle13").click(function(){
+    $(".fl13").slideToggle("slow");
+  });
+
+  $("#toggle14").click(function(){
+    $(".fl14").slideToggle("slow");
+  });
+
+  $("#toggle15").click(function(){
+    $(".fl15").slideToggle("slow");
+  });
+
   $(".box").hover(function(){
     $(this).css({"background-color": "rgba(255, 255, 255, 0.7)"});
     }, function(){
